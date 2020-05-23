d6c45e76766dc127b18e874ce4e54974db6cf905
lulu731 <lulu731lulu@hotmail.fr>
2020-05-23 15:18:19 +0200

    TEST : add test for App.

--- src/components/card/TitleCard.js
+++ src/components/card/TitleCard.js
@@ -3,8 +3,8 @@ import 'tachyons';
 
 const TitleCard = () => {
   return (
-    <article class="center mw5 mw6-ns br3 hidden ba b--black-10 mv4">
-      <h6 class="f6 bg-near-white br3 br black-60 mv0 pv2 ph3">
+    <article className="center mw5 mw6-ns br3 hidden ba b--black-10 mv4">
+      <h6 className="f6 bg-near-white br3 br black-60 mv0 pv2 ph3">
         Title of card
       </h6>
     </article>
