47dd12ca4d219b4faefc1003256ef367551108dd
lulu731 <lulu731lulu@hotmail.fr>
2020-05-24 18:11:14 +0200

    Implement Panels init.

--- src/components/panels/Panel.js
+++ src/components/panels/Panel.js
@@ -5,16 +5,33 @@ import 'tachyons';
 class Panel extends React.Component {
   constructor(props) {
     super(props);
-    if (props.uAndI === 'u&i') {this.bg = 'bg-red'};
+    switch (props.uAndI) {
+      case 'u&i':
+        this.bg = 'bg-red';
+        this.text = 'Urgent & Important';
+        break;
+      case 'nu&i':
+        this.bg = 'bg-blue';
+        this.text = 'not Urgent & Important';
+        break;
+      case 'u&ni':
+        this.bg = 'bg-light-blue';
+        this.text = 'Urgent & not Important';
+        break;
+      case 'nu&ni':
+        this.bg = 'bg-green';
+        this.text = 'not Urgent & not Important';
+        break;
+    }
     this.tasks = props.tasksArray;
   };
 
   render() {
     return (
       <div>
-        <div className="fl w-50 h-100 ba {this.bg} tc"
+        <div className="fl w-50 h-100 ba tc" className = {this.bg}
           style={{zIndex:-1}}>
-          Urgent & Important
+          {this.text}
           {this.tasks.map((item, i) => {
             return (
               <TitleCard
--- src/components/panels/Panels.js
+++ src/components/panels/Panels.js
@@ -1,43 +1,26 @@
 import React from 'react';
-import Card from '../card/Card.js'
-import TitleCard from '../card/TitleCard.js'
+import Panel from '../panels/Panel.js'
+// import TitleCard from '../card/TitleCard.js'
 import 'tachyons';
 
-const tasks = ['task0', 'task1', 'task2'];
+const tasksUI = ['TaskUI0', 'task1', 'task2'];
+const tasksNUI = ['TaskNUI0', 'task1'];
+const tasksUNI = ['TaskUNI0', 'task1', 'task2'];
+const tasksNUNI = ['TaskNUNI0', 'task1', 'task2'];
+
 const Panels = () => {
   return (
     <div>
       <div style={{zIndex:-1, position: "absolute",
       left: 0 , right: 0}} >
-      <div className="vh-50">
-        <div className="fl w-50 h-100 ba bg-red tc"
-          style={{zIndex:-1}}>
-          Urgent & Important
-          {tasks.map((item, i) =>{
-            return (
-              <TitleCard
-                key = {i}
-                title = {item}/>
-            )
-          })}
+        <div className="vh-50">
+          <Panel uAndI = 'u&i' tasksArray = {tasksUI}/>
+          <Panel uAndI = 'nu&i' tasksArray = {tasksNUI}/>
         </div>
-        <div className="fl ba w-50 h-100 bg-blue tc"
-          style={{zIndex:-1}}>
-          not Urgent & Important
+        <div className="vh-50">
+          <Panel uAndI = 'u&ni' tasksArray = {tasksUNI}/>
+          <Panel uAndI = 'nu&ni' tasksArray = {tasksNUNI}/>
         </div>
-      </div>
-      <div className="vh-50">
-        <div className="fl w-50 h-100 ba bg-light-blue tc"
-          style={{zIndex:-1}}>
-          Urgent & not Important
-          <TitleCard />
-          <TitleCard />
-          <TitleCard />
-        </div>
-        <div className="fl ba w-50 h-100 bg-green tc">
-          not Urgent & not Important
-        </div>
-      </div>
     </div>
   </div>
   )
